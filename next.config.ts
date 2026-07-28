902463ecb3cf49d87682a68dea7e3551b44668bf
Xennus352 <xennus.dev@gmail.com>
2026-07-28 13:22:28 +0630

    server configure

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "50mb",
+      allowedOrigins: [
+        "library.ucstaungoo.edu.mm",
+        "*.ucstaungoo.edu.mm",
+        "10.23.23.110",
+        "10.23.23.110:3000",      
+      ],
     },
   },
 };
